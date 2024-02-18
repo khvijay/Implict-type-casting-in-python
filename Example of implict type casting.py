2b9ffaa90454cecc6e6542c3585428b919c62caf
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-18 18:36:54 +0530

    Add files via upload

---
+++ Example of implict type casting.py
@@ -0,0 +1,13 @@
+# python automatically converts 
+# a to int
+a = 7
+print(type(a))
+
+# python automatically converts b to float
+b = 3.0
+print(type(b))
+
+# python automatically converts c to float as it is float addition 
+c = a + b
+print(c)
+print(type(c))
